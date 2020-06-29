04529956675048f83bcda7689a1e3767ba647f82
Nikolay Shvets <47063312+NikolayShvets@users.noreply.github.com>
2020-06-29 11:34:15 +0300

    Add files via upload

--- logger/strlogger.py
+++ logger/strlogger.py
@@ -1,8 +1,13 @@
+"""
+Модуль, инициализирующий объект логгера из модуля <logging>
+в соответствии с параметрами из <logger/logger.conf>
+"""
 import logging.config
 
 logging.config.fileConfig(fname='logger/logger.conf', disable_existing_loggers=True)
 strlogger = logging.getLogger(__name__)
 
+# словарь функций логгера, ключи - уровни сообщения
 logger_dict = {
     "DEBUG": strlogger.debug,
     "INFO": strlogger.info,
